4f8eadbd033d2bbe944a34d1ff20ca08e496bbe6
yangyusen <yusen.yang@hand-china.com>
2016-03-31 19:37:49 +0800

    添加Footer action,store,理解Flux,alt

--- app/components/app.js
+++ app/components/app.js
@@ -2,6 +2,7 @@
  * Created by hasee on 2016/3/27.
  */
 import React from 'react';
+import Footer from './Footer';
 //import {RouteHandler} from 'react-router';
 
 class App extends React.Component {
@@ -9,6 +10,7 @@ class App extends React.Component {
         return (
             <div>
                 {this.props.children}
+                <Footer />
             </div>
         );
     }
--- public/js/bundle.js
+++ public/js/bundle.js
@@ -5,11 +5,75 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }(); /*** Created by yangyusenhit on 2016/3/31.*/
+
+
+var _alt = require('../alt');
+
+var _alt2 = _interopRequireDefault(_alt);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var FooterActions = function () {
+    function FooterActions() {
+        _classCallCheck(this, FooterActions);
+
+        this.generateActions('getTopCharactersSuccess', 'getTopCharactersFail');
+    }
+
+    _createClass(FooterActions, [{
+        key: 'getTopCharacters',
+        value: function getTopCharacters() {
+            var _this = this;
+
+            $.ajax({ url: '/api/characters/top' }).done(function (data) {
+                _this.actions.getTopCharactersSuccess(data);
+            }).fail(function (jqXhr) {
+                _this.actions.getTopCharactersFail(jqXhr);
+            });
+        }
+    }]);
+
+    return FooterActions;
+}();
+
+exports.default = _alt2.default.createActions(FooterActions);
+
+},{"../alt":2}],2:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _alt = require('alt');
+
+var _alt2 = _interopRequireDefault(_alt);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = new _alt2.default(); /**
+                                        * Created by yangyusenhit on 2016/3/25.
+                                        */
+
+},{"alt":"alt"}],3:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
+
+var _Footer = require('./Footer');
+
+var _Footer2 = _interopRequireDefault(_Footer);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -39,7 +103,8 @@ var App = function (_React$Component) {
             return _react2.default.createElement(
                 'div',
                 null,
-                _react2.default.createElement(RouteHandler, null)
+                this.props.children,
+                _react2.default.createElement(_Footer2.default, null)
             );
         }
     }]);
@@ -49,7 +114,189 @@ var App = function (_React$Component) {
 
 exports.default = App;
 
-},{"react":"react"}],2:[function(require,module,exports){
+},{"./Footer":4,"react":"react"}],4:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactRouter = require('react-router');
+
+var _FooterStore = require('../stores/FooterStore');
+
+var _FooterStore2 = _interopRequireDefault(_FooterStore);
+
+var _FooterActions = require('../actions/FooterActions');
+
+var _FooterActions2 = _interopRequireDefault(_FooterActions);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; } /**
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                * Created by yangyusenhit on 2016/3/31.
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                */
+
+
+var Footer = function (_React$Component) {
+    _inherits(Footer, _React$Component);
+
+    function Footer(props) {
+        _classCallCheck(this, Footer);
+
+        var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(Footer).call(this, props));
+
+        _this.state = _FooterStore2.default.getState();
+        _this.onChange = _this.onChange.bind(_this);
+        return _this;
+    }
+
+    //在组件render之后执行
+
+
+    _createClass(Footer, [{
+        key: 'componentDidMount',
+        value: function componentDidMount() {
+            _FooterStore2.default.listen(this.onChange);
+            _FooterActions2.default.getTopCharacters();
+        }
+
+        //在组件销毁之前执行
+
+    }, {
+        key: 'componentWillUnmount',
+        value: function componentWillUnmount() {
+            _FooterStore2.default.unlisten(this.onChange);
+        }
+    }, {
+        key: 'onChange',
+        value: function onChange(state) {
+            this.setState(state);
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+            var leaderboardCharacters = this.state.characters.map(function (character) {
+                return _react2.default.createElement(
+                    'li',
+                    { key: character.characterId },
+                    _react2.default.createElement(
+                        _reactRouter.Link,
+                        { to: '/characters/' + character.characterId },
+                        _react2.default.createElement('img', { className: 'thumb-md',
+                            src: 'http://image.eveonline.com/Character/' + character.characterId + '_128.jpg' })
+                    )
+                );
+            });
+
+            return _react2.default.createElement(
+                'footer',
+                null,
+                _react2.default.createElement(
+                    'div',
+                    { className: 'container' },
+                    _react2.default.createElement(
+                        'div',
+                        { className: 'row' },
+                        _react2.default.createElement(
+                            'div',
+                            { className: 'col-sm-5' },
+                            _react2.default.createElement(
+                                'h3',
+                                { className: 'lead' },
+                                _react2.default.createElement(
+                                    'strong',
+                                    null,
+                                    'Information'
+                                ),
+                                ' and ',
+                                _react2.default.createElement(
+                                    'strong',
+                                    null,
+                                    'Copyright'
+                                )
+                            ),
+                            _react2.default.createElement(
+                                'p',
+                                null,
+                                'Powered by ',
+                                _react2.default.createElement(
+                                    'strong',
+                                    null,
+                                    'Node.js'
+                                ),
+                                ',',
+                                _react2.default.createElement(
+                                    'strong',
+                                    null,
+                                    'MongoDB'
+                                ),
+                                ' and ',
+                                _react2.default.createElement(
+                                    'strong',
+                                    null,
+                                    'React'
+                                ),
+                                ' with Flux architecture and server-side rendering.'
+                            ),
+                            _react2.default.createElement(
+                                'p',
+                                null,
+                                'You may view the ',
+                                _react2.default.createElement(
+                                    'a',
+                                    { href: 'https://github.com/EthanYangHIT/newedenfaces-react' },
+                                    'Source Code'
+                                ),
+                                ' behind this project on GitHub.'
+                            ),
+                            _react2.default.createElement(
+                                'p',
+                                null,
+                                '© 2015 Ethan Yang.'
+                            )
+                        ),
+                        _react2.default.createElement(
+                            'div',
+                            { className: 'col-sm-7 hidden-sx' },
+                            _react2.default.createElement(
+                                'h3',
+                                { className: 'lead' },
+                                _react2.default.createElement(
+                                    'strong',
+                                    null,
+                                    'Leaderboard'
+                                ),
+                                '  Top 5 Characters'
+                            ),
+                            _react2.default.createElement(
+                                'ul',
+                                { className: 'list-inline' },
+                                leaderboardCharacters
+                            )
+                        )
+                    )
+                )
+            );
+        }
+    }]);
+
+    return Footer;
+}(_react2.default.Component);
+
+exports.default = Footer;
+
+},{"../actions/FooterActions":1,"../stores/FooterStore":8,"react":"react","react-router":"react-router"}],5:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
@@ -98,7 +345,7 @@ var Home = function (_React$Component) {
 
 exports.default = Home;
 
-},{"react":"react"}],3:[function(require,module,exports){
+},{"react":"react"}],6:[function(require,module,exports){
 'use strict';
 
 var _react = require('react');
@@ -129,7 +376,7 @@ _reactRouter2.default.run(_routes2.default, _reactRouter2.default.HistoryLocatio
      * Created by yangyusenhit on 2016/3/25.
      */
 
-},{"./routes":4,"react":"react","react-router":"react-router"}],4:[function(require,module,exports){
+},{"./routes":7,"react":"react","react-router":"react-router"}],7:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
@@ -167,4 +414,53 @@ exports.default = _react2.default.createElement(
     _react2.default.createElement(_reactRouter.Route, { path: '/', component: _Home2.default })
 );
 
-},{"./components/App":1,"./components/Home":2,"react":"react","react-router":"react-router"}]},{},[3]);
+},{"./components/App":3,"./components/Home":5,"react":"react","react-router":"react-router"}],8:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }(); /**
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      * Created by yangyusenhit on 2016/3/31.
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      */
+
+
+var _alt = require('../alt');
+
+var _alt2 = _interopRequireDefault(_alt);
+
+var _FooterActions = require('../actions/FooterActions');
+
+var _FooterActions2 = _interopRequireDefault(_FooterActions);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var FooterStore = function () {
+    function FooterStore() {
+        _classCallCheck(this, FooterStore);
+
+        this.bindActions(_FooterActions2.default);
+        this.characters = [];
+    }
+
+    _createClass(FooterStore, [{
+        key: 'onGetTopCharactersSuccess',
+        value: function onGetTopCharactersSuccess(data) {
+            this.characters = data.slice(0, 5);
+        }
+    }, {
+        key: 'onGetTopCharactersFail',
+        value: function onGetTopCharactersFail(jqXhr) {
+            toastr.error(jqXhr.responseJSON && jqXhr.responseJSON.message || jqXhr.responseText || jqXhr.statusText);
+        }
+    }]);
+
+    return FooterStore;
+}();
+
+exports.default = _alt2.default.createStore(FooterStore);
+
+},{"../actions/FooterActions":1,"../alt":2}]},{},[6]);
